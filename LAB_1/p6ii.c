927c847525b2682b21fbb9743b52040280a8c9d4
Jeeldobariya33 <jeeldobariya33@gmail.com>
2026-06-09 11:08:04 +0530

    Add implementations for prime factorization with and without recursion

---
+++ LAB_1/p6ii.c
@@ -0,0 +1,28 @@
+//print prime factors of a number without recursion
+#include<stdio.h>
+#include<time.h>
+void primeFactors(int n){
+    while(n <= 1)
+        return;
+    for(int i = 2; i <= n; i++){
+        if(n % i == 0){
+            printf("%d ", i);
+            n = n / i;
+            break;
+        }
+    }
+}
+
+int main(){
+    int n;
+    clock_t start, end;
+    start = clock();
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Prime factors of %d are: ", n);
+    primeFactors(n);
+    end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("\nTime taken to calculate prime factors: %f seconds", time_taken);
+    return 0;
+}
